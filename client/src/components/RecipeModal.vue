9c491db02e270c765275a84b746f08a7d7c4505b
Justin Morrow <justintwomorrow@gmail.com>
2025-04-22 15:57:23 -0600

    some more style on modal

--- client/src/components/RecipeModal.vue
+++ client/src/components/RecipeModal.vue
@@ -7,39 +7,37 @@ const recipe = computed(() => AppState.activeRecipe)
 
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-7">
 
-        <div class="modal" id="recipeModal" tabindex="-1">
-          <div class="modal-dialog">
-            <div v-if="recipe" class="modal-content">
-              <div class="modal-body">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-6">
-                      <div><img :src="recipe.img" alt="" class="modal-img"></div>
-                    </div>
-                    <div class="col-6">
-                      <h5 class="modal-title">{{ recipe.title }}</h5>
-                      <span class="ms-4 bg-shaq-pink ">{{ recipe.category }}</span>
-                      <p>{{ recipe.instructions }}</p>
-                      <span>{{ recipe.createdAt }}</span>
-                    </div>
-                  </div>
-                </div>
+
+  <div class="modal" id="recipeModal" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+      <div v-if="recipe" class="modal-content">
+        <div class="modal-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-6">
+                <div><img :src="recipe.img" alt="" class="modal-img"></div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+              <div class="col-6">
+                <span class="d-flex">
+                  <h5 class="modal-title">{{ recipe.title }}</h5>
+                  <span class="ms-3 bg-shaq-pink ">{{ recipe.category }}</span>
+                </span>
+                <div>{{ recipe.creator_id }}</div>
+                <p>{{ recipe.instructions }}</p>
+                <span>{{ recipe.createdAt }}</span>
               </div>
             </div>
           </div>
         </div>
-
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
+
 
 
 
@@ -51,8 +49,8 @@ const recipe = computed(() => AppState.activeRecipe)
 
 <style lang="scss" scoped>
 .modal-img {
-  height: 30dvh;
-  width: 22dvh;
+  height: 40dvh;
+  width: 35dvh;
 
 }
 
